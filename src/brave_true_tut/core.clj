2430f8185175d04129816956011bd67b9c2a9faa
charlieflowers <cflowers@gmail.com>
2015-08-20 12:58:02 -0400

    some really old work in progress

--- src/brave_true_tut/core.clj
+++ src/brave_true_tut/core.clj
@@ -81,16 +81,118 @@
         x
         (recur xs (+ accumulated-size (:size x)))))))
 
-(defn hit-reduce
-  [asym-body-parts]
+(defn hit-reduce [asym-body-parts]
   (let [sym-parts (charlie asym-body-parts)
         size-sum (reduce + 0 (map :size sym-parts))
         target (inc (rand size-sum))]
-    (reduce (fn [accsize part]
-              (if (:size accsize)
-                accsize
-                (let [newsize (+ accsize (:size part))]
-                  (if (> newsize target)
-                    part
-                    newsize)))
-              ) 0 sym-parts)))
+    (reduce
+     (fn [accsize part]
+       (if (:size accsize)
+         part
+         (let [newsize (+ accsize (:size part))]
+           (if (> newsize target)
+             part
+             newsize)))
+       ) 0 sym-parts)))
+
+(defn big-list
+  []
+  (charlie asym-hobbit-body-parts))
+
+
+(matching-part {:name "left-ass-cheek" :size 42})
+
+; Rest of this file is Joy of Clojure experimentation in the repl
+;; (defn xors [max-x max-y]  (for [x (range max-x) y (range max-y)] [x y (bit-xor x y)]))
+
+;; (bit-xor 1 2)
+
+;; (xors 2 2)
+
+;; (def frame (java.awt.Frame.))
+
+;; frame
+
+;; (for [method (seq (.getMethods java.awt.Frame))
+;;       :let [method-name (.getName method)]
+;;       :when (re-find #"Vis" method-name)]
+;;   method-name)
+
+;; (.isVisible frame)
+
+;; (.setVisible frame true)
+;; (.setSize frame (java.awt.Dimension. 256 256))
+;; (.setLocation frame 300 300)
+
+;; (def gfx (.getGraphics frame))
+
+;; (.fillRect gfx 100 100 50 75)
+
+;; (.setColor gfx (java.awt.Color. 255 128 0))
+;; (.fillRect gfx 100 150 75 50)
+
+(defn neighbors
+  ([size yx] (neighbors [[-1 0] [1 0] [0 -1] [0 1]] size yx))
+  ([deltas size yx]
+     (filter (fn [new-yx]
+               (every? #(< -1 % size) new-yx))
+             (map #(map + yx %) deltas))))
+;; (peek)
+
+;; (pop)
+
+;; (last)
+(def schedule
+  (conj clojure.lang.PersistentQueue/EMPTY
+        :wake-up :shower :brush-teeth))
+
+(defn shite
+  []
+  (println "shite!"))
+
+(defn shitola
+  []
+  (println "shitola!"))
+
+
+(defn random-ints
+  [limit]
+  (lazy-seq
+   (println "realizing random number")
+   (cons (rand-int limit)
+         (random-ints limit))))
+
+;; (def rands (take 10 (random-ints 50)))
+
+;; (nth rands 3)
+
+(def foo (next (random-ints 50)))
+
+(println "done with foo")
+
+(def bar (rest (random-ints 50)))
+
+(println "done with bar")
+
+;; Now, from Clojure Programming, the Show Info example
+(ns com.clojurebook.fn-browser
+  (:import (javax.swing JList JFrame JScrollPane JButton)
+           java.util.Vector))
+
+(defonce fn-names (->> (ns-publics 'clojure.core)
+                       (map key)
+                       sort
+                       Vector.
+                       JList.))
+
+(defn show-info [] )
+
+(defonce window (doto (JFrame. "\"Interactive Development!\"")
+                  (.setSize (java.awt.Dimension. 400 300))
+                  (.setLocation 300 300)
+                  (.add (JScrollPane. fn-names))
+                  (.add java.awt.BorderLayout/SOUTH
+                        (doto (JButton. "Show Info")
+                          (.addActionListener (reify java.awt.event.ActionListener
+                                                (actionPerformed [_ e] (show-info))))))
+                  (.setVisible true)))
